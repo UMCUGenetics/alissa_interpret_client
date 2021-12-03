bde7c141fbc4b3287d67d10ad9a6893cb23fb4ed
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2021-12-03 13:41:11 +0100

    Update manual_review_analyses and cleanup code

---
+++ scripts/README.md
@@ -0,0 +1,2 @@
+# Alissa Interpret Public API Python Client - Scripts
+This folder contains a collection of scripts that use `alissa_interpret_client` specifically developed for UMCU usescases.
--- scripts/manual_review_analyses.py
+++ scripts/manual_review_analyses.py
@@ -2,69 +2,99 @@
 
 ToDo:
     - Discuss with Agilent about directly filtering 'manual review' variants. Exporting all variants takes a lot of time.
-    - check large analyses (variant count > 20000)
-    - Cleanup code (line length and duplicated code)
 """
 
 from requests.exceptions import HTTPError
 import time
+from datetime import datetime
 
 from alissa_interpret_client.alissa_interpret import AlissaInterpret
 
-client = AlissaInterpret(
-    base_uri='',
-    client_id='',
-    client_secret='',
-    username='',
-    password=''
-)
+import config
 
-print(f"analysis_reference\tanalysis_type\tcreated_on\tlast_updated_on\tmanual_review_count")
 
-for analysis in client.get_analyses(status='IN_PROGRESS', created_after='2021-01-01T00:00:00.000+0000'):
-    if not analysis['classificationTreeName']:  # Skip analysis without classification tree
-        continue
+def get_analysis_data(client, type, id):
+    if type == 'PATIENT':
+        return client.get_patient_analyses(id)
+    elif type == 'INHERITANCE':
+        return client.get_inheritance_analyses(id)
 
-    if analysis['analysisType'] == 'PATIENT':
-        patient_analysis = client.get_patient_analyses(analysis['id'])
-        variant_count = sum([lab_result['analysisVariantCount']['molecularVariantCount'] for lab_result in patient_analysis['labResults']])
-        if variant_count > 20000:  # skip large analyses
-            print(f"{analysis['reference']}\t{analysis['analysisType']}\t{analysis['createdOn'][0:10]}\t{analysis['lastUpdatedOn'][0:10]}\tskipped_large_analysis")
+
+def post_variants_export(client, type, id):
+    if type == 'PATIENT':
+        return client.post_patient_analyses_variants_export(id)['exportId']
+    elif type == 'INHERITANCE':
+        return client.post_inheritance_analyses_variants_export(id)['exportId']
+
+
+def get_variants_export(client, type, id, export_id):
+    if type == 'PATIENT':
+        return client.get_patient_analyses_variants_export(id, export_id)
+    elif type == 'INHERITANCE':
+        return client.get_inheritance_analyses_variants_export(id, export_id)
+
+
+if __name__ == '__main__':
+    client = AlissaInterpret(
+        base_uri=config.alissa_base_uri, client_id=config.alissa_client_id, client_secret=config.alissa_client_secret,
+        username=config.alissa_username, password=config.alissa_password
+    )
+
+    # Print header
+    print((
+        "analysis_reference\tanalysis_type\tcreated_on\tlast_updated_on\t"
+        "molecular_variant_count\tcnv_count\tmanual_review_count"
+    ))
+
+    # Get in progress analyses and < 1 year old.
+    filter_date = datetime.now().replace(year=datetime.now().year - 1)
+    for analysis in client.get_analyses(status='IN_PROGRESS', created_after=filter_date.strftime('%Y-%m-%dT%H:%M:%S.%f+0000')):
+        analysis_id = analysis['id']
+        analysis_type = analysis['analysisType']
+
+        # Skip analysis without classification tree or PATIENT/INHERITANCE analysis type
+        if not analysis['classificationTreeName'] or analysis_type not in ['PATIENT', 'INHERITANCE']:
             continue
 
-        export_id = client.post_patient_analyses_variants_export(analysis['id'])['exportId']
-        export = None
+        # Get analysis data and calculate variant counts
+        analysis_data = get_analysis_data(client, analysis_type, analysis_id)
+        mol_var_count = sum(
+            [lab_result['analysisVariantCount']['molecularVariantCount'] for lab_result in analysis_data['labResults']]
+        )
+        cnv_count = sum(
+            [lab_result['analysisVariantCount']['copyNumberVariationCount'] for lab_result in analysis_data['labResults']]
+        )
 
+        # skip analysis with cnv results
+        if cnv_count >= 1:
+            print((
+                f"{analysis['reference']}\t{analysis_type}\t{analysis['createdOn'][0:10]}\t"
+                f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\tskipped_analysis_contains_cnv"
+            ))
+            continue
+
+        # skip analysis with a lot of variants (slow export)
+        if mol_var_count > 1000:
+            print((
+                f"{analysis['reference']}\t{analysis_type}\t{analysis['createdOn'][0:10]}\t"
+                f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\tskipped_large_analysis"
+            ))
+            continue
+
+        # Export variants and count manual review
+        export_id = post_variants_export(client, analysis_type, analysis_id)
+        export = None
         while export is None:
             try:
-                time.sleep(variant_count/100)  # 5 sec delay to request exported report.
-                export = client.get_patient_analyses_variants_export(analysis['id'], export_id)
+                time.sleep(mol_var_count/100 + 1)  # delay to request exported report, min 1 sec.
+                export = get_variants_export(client, analysis_type, analysis_id, export_id)
             except HTTPError:
                 pass
 
         manual_review = ['Y,manual review' in variant['classificationTreeLabelsScore']['labels'] for variant in export]
         manual_review_count = sum(manual_review)
 
-        print(f"{analysis['reference']}\t{analysis['analysisType']}\t{analysis['createdOn'][0:10]}\t{analysis['lastUpdatedOn'][0:10]}\t{manual_review_count}")
-
-    elif analysis['analysisType'] == 'INHERITANCE':
-        inheritance_analysis = client.get_inheritance_analyses(analysis['id'])
-        variant_count = sum([lab_result['analysisVariantCount']['molecularVariantCount'] for lab_result in inheritance_analysis['labResults']])
-        if variant_count > 20000:  # skip large analyses
-            print(f"{analysis['reference']}\t{analysis['analysisType']}\t{analysis['createdOn'][0:10]}\t{analysis['lastUpdatedOn'][0:10]}\tskipped_large_analysis")
-            continue
-
-        export_id = client.post_inheritance_analyses_variants_export(analysis['id'])['exportId']
-        export = None
-
-        while export is None:
-            try:
-                time.sleep(variant_count/100)  # 5 sec delay to request exported report.
-                export = client.get_inheritance_analyses_variants_export(analysis['id'], export_id)
-            except HTTPError:
-                pass
-
-        manual_review = ['Y,manual review' in variant['classificationTreeLabelsScore']['labels'] for variant in export]
-        manual_review_count = sum(manual_review)
-
-        print(f"{analysis['reference']}\t{analysis['analysisType']}\t{analysis['createdOn'][0:10]}\t{analysis['lastUpdatedOn'][0:10]}\t{manual_review_count}")
+        print((
+            f"{analysis['reference']}\t{analysis_type}\t{analysis['createdOn'][0:10]}\t"
+            f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\t{manual_review_count}"
+        ))
