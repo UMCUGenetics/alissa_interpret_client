89059f69f5f7ed63d91a3aaba726f01b974841ed
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2021-12-14 16:22:04 +0100

    Add all manual review types

--- scripts/manual_review_analyses.py
+++ scripts/manual_review_analyses.py
@@ -3,9 +3,8 @@
 ToDo:
     - Discuss with Agilent about directly filtering 'manual review' variants. Exporting all variants takes a lot of time.
 """
-
-from requests.exceptions import HTTPError
 import time
+from requests.exceptions import HTTPError
 from datetime import datetime
 
 from alissa_interpret_client.alissa_interpret import AlissaInterpret
@@ -35,6 +34,7 @@ def get_variants_export(client, type, id, export_id):
 
 
 if __name__ == '__main__':
+    # Create Alissa connection
     client = AlissaInterpret(
         base_uri=config.alissa_base_uri, client_id=config.alissa_client_id, client_secret=config.alissa_client_secret,
         username=config.alissa_username, password=config.alissa_password
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     # Print header
     print((
         "analysis_reference\tanalysis_type\tcreated_on\tlast_updated_on\t"
-        "molecular_variant_count\tcnv_count\tmanual_review_count"
+        "molecular_variant_count\tcnv_count\tmanual_review_count_Y\tmanual_review_count_Y2\tmanual_review_count_Y3"
     ))
 
     # Get in progress analyses and < 1 year old.
@@ -74,7 +74,7 @@ if __name__ == '__main__':
             continue
 
         # skip analysis with a lot of variants (slow export)
-        if mol_var_count > 1000:
+        if mol_var_count > 10000:
             print((
                 f"{analysis['reference']}\t{analysis_type}\t{analysis['createdOn'][0:10]}\t"
                 f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\tskipped_large_analysis"
@@ -91,10 +91,21 @@ if __name__ == '__main__':
             except HTTPError:
                 pass
 
-        manual_review = ['Y,manual review' in variant['classificationTreeLabelsScore']['labels'] for variant in export]
-        manual_review_count = sum(manual_review)
+        # Count manual review labels
+        manual_review_count = [0, 0, 0]  # manual_review labes Y, Y2, Y3 (rare)
+        for variant in export:
+            variant_labels = variant['classificationTreeLabelsScore']['labels'].lower()
 
+            if 'y,manual review' in variant_labels:
+                manual_review_count[0] += 1
+            if 'y2,manual review' in variant_labels:
+                manual_review_count[1] += 1
+            if 'y3 (rare),manual review' in variant_labels:
+                manual_review_count[2] += 1
+
+        # Print result
         print((
             f"{analysis['reference']}\t{analysis_type}\t{analysis['createdOn'][0:10]}\t"
-            f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\t{manual_review_count}"
+            f"{analysis['lastUpdatedOn'][0:10]}\t{mol_var_count}\t{cnv_count}\t"
+            f"{manual_review_count[0]}\t{manual_review_count[1]}\t{manual_review_count[2]}"
         ))
